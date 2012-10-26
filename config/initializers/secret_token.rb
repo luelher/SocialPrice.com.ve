7f1a065094dff35da6a2900cac02bd4a054114d4
Luis Hernández <luelher@gmail.com>
2012-10-25 23:12:23 -0430

    Primer Commit SocialPrice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialpriceComVe::Application.config.secret_token = 'b8640249c89a26e88aa42eaaaa2c3afa3c6456aafa0050148eafa28b30856436e499b7fedc0b0ffe61e49a653f59f63e0f2b29e4cde4c66b4c7cc6e86f894d46'
